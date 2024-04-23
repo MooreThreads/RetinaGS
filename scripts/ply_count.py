d9960529035fd257927257b2c498e2ee1c64776f
root <shengyi.chen@mthreads.com>
2024-04-23 13:37:18 +0800

    add scale count

--- scripts/ply_count.py
+++ scripts/ply_count.py
@@ -20,30 +20,48 @@ def load_ply_scales(path : str):
     
     return scales
 
-def plot_prefix(scales, save_plot_path):
+def plot_prefix(scales, save_plot_path, save_plot_2_path):
+    # 计算体积
+    volumes = np.prod(scales, axis=1)
+    volumes_sorted = np.sort(volumes)
+    print('mean product of activated gs: ', str(volumes.mean().item()))
     # 排序
     scales = np.sort(scales, axis=1)
     scales_sorted = np.sort(scales, axis=0)
     # 计算前缀和
     cumulative_counts = np.arange(1, scales_sorted.shape[0] + 1)
 
-    # 作图
-    plt.figure()
+    # 作图    
     indices = np.linspace(0, scales_sorted.shape[0]-1, 1000, dtype=int) 
+    # 三轴统计
+    plt.figure()
     for i in range(scales.shape[1]):
         # plt.plot(np.squeeze(scales_sorted[indices, i]), np.squeeze(cumulative_counts[indices]), label=f'Row {i+1}')
-        plt.semilogx(np.squeeze(scales_sorted[indices, i]), np.squeeze(cumulative_counts[indices]), label=f'Row {i+1}')
-
+        plt.semilogx(np.squeeze(scales_sorted[indices, i]), np.squeeze(cumulative_counts[indices]), label=f'axis {i+1}')
+        print("mean of axis ", str(i+1), " ", str(scales_sorted[:, i].mean().item()))
+        
     plt.legend()
     plt.savefig(save_plot_path)
     print("save at " + save_plot_path)
+    # 体积统计
+    plt.figure()
+    plt.semilogx(np.squeeze(volumes_sorted[indices]), np.squeeze(cumulative_counts[indices]), label=f'volume', color='k')
+    plt.legend()
+    plt.savefig(save_plot_2_path)
+    print("save at " + save_plot_2_path)
+    
 
     
 
 if __name__ == '__main__':
-    ply_path = '/root/Nerf/Predict/mipnerf360/room/A100_colmap-default_gaussian-splatting-default_mipnerf360-room/point_cloud/iteration_30000/point_cloud_only_activated.ply'
-    save_plot_path = '/root/Nerf/Code/HT/DenseGaussian/backup/prefix_sums.png'
+    # ply_path = '/root/Nerf/Predict/indoor/demo_room_luchao/colmap_dense_None-PM_None-xyz_pointcloud_sample_rate-16_huber_loss_r-1_train-with-epoch/point_cloud/iteration_31136/point_cloud_only_activated.ply'
+    # ply_path = '/root/Nerf/Predict/indoor/demo_room_luchao/colmap_dense_None-PM_None-xyz_pointcloud_sample_rate-16_huber_loss_r-1_scale-product_train-with-epoch/point_cloud/iteration_31136/point_cloud_only_activated.ply'
+    # ply_path = '/root/Nerf/Predict/indoor/demo_room_luchao/colmap_dense_None-PM_None-xyz_pointcloud_sample_rate-16_huber_loss_r-1_scale-sum_train-with-epoch/point_cloud/iteration_31136/point_cloud_only_activated.ply'
+    # ply_path = '/root/Nerf/Predict/indoor/demo_room_luchao/colmap_dense_None-PM_None-xyz_pointcloud_sample_rate-16_huber_loss_r-1_scale-square-sum_train-with-epoch/point_cloud/iteration_31136/point_cloud_only_activated.ply'
+    ply_path = '/root/Nerf/Predict/indoor/demo_room_luchao/colmap_default_gs_default_r_1/point_cloud/iteration_30000/point_cloud_only_activated.ply'
+    save_plot_path = '/root/Nerf/Code/DenseGaussian/backup/demo_room/default.png'
+    save_plot_2_path = '/root/Nerf/Code/DenseGaussian/backup/demo_room/default_volume.png'
     scales = load_ply_scales(ply_path)
     scales = np.exp(scales)
-    plot_prefix(scales, save_plot_path)
+    plot_prefix(scales, save_plot_path, save_plot_2_path)
     
