6db4ea913460001d949ca85d505e387658469e3a
root <shengyi.chen@mthreads.com>
2024-09-26 00:24:24 +0800

    update start

--- multi_node_cmds/start_multi_node.sh
+++ multi_node_cmds/start_multi_node.sh
@@ -16,7 +16,7 @@ conda init
 source ~/.bashrc
 # conda activate dense
 # pip install tensorboad
-conda activate gaussian_splatting
+conda activate retina_gs
 
 env
 
@@ -25,15 +25,10 @@ export NCCL_SOCKET_TIMEOUT=6000
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
-torchrun --nnodes=$NNODES --node_rank=$NODE_RANK --nproc_per_node=8 \
-    --master_addr=$MASTER_ADDR --master_port=39527  \
-    train_MP_tree_partition.py \
-        -s path/2/data \
-        -m path/2/model \
-        -r 1 --epochs 40  --eval --points3D $NAME_OF_PLY_FILE \
-        --scaling_lr_init 0.0005 --scaling_lr_final 0.000005 --position_lr_init 0.00000016 --position_lr_final 0.0000000016 \
-        --densify_until_iter 0 --SKIP_SPLIT --SKIP_CLONE --opacity_reset_interval 3000 --densification_interval 100 \
-        --bvh_depth 4 \
-        --max_batch_size 4  --max_load 4 \
-        --EVAL_INTERVAL_EPOCH 1 --SAVE_INTERVAL_EPOCH 1 --SAVE_INTERVAL_ITER 10000 \
-        --CKPT_MAX_NUM 2
+torchrun --nnodes=$NNODES --node_rank=$NODE_RANK --nproc_per_node=8 --master_addr=$MASTER_ADDR --master_port=39527  \
+    main.py -s data/Garden-1.6k -m model/Garden-1.6k_62M \
+        --bvh_depth 4 --MAX_BATCH_SIZE 4  --MAX_LOAD 8 \
+        -r 1 --eval \
+        --position_lr_init 0.0000016 --position_lr_final 0.000000016 --densify_until_iter 0 \
+        --points3D MVS_points3D --pointcloud_sample_rate 1 \
+        --iterations 60000
